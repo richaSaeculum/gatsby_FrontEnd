191d2b715113b72e6de5367e5f7aafec59124bf5
Shyam Kathiriya <91935832+shyam-kathiriya@users.noreply.github.com>
2022-11-10 10:56:36 +0530

    Update [tag].js

--- pages/tags/[tag].js
+++ pages/tags/[tag].js
@@ -12,7 +12,7 @@ export async function getServerSideProps(context) {
   let { tagid, tag } = context.query;
 
   // const { data } = await axios.get(`https://gatsby.saeculumsolutions.com/wp-json/wp/v2/posts?categories=${tagid}&_embed`);
-  const { data } = await axios.get(`http://52.22.62.85:9030/api/user/articles/list?category=${tagid}`);
+  const { data } = await axios.get(`http://52.22.62.85:9030/api/user/articles/list?category=${tagid}&status=publish`);
 
   return {
     props: {
